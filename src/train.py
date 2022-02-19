d78463f8bf671d0d63458130fa134e0fdbe08d36
Vince <vincivenv@gmail.com>
2022-02-19 23:32:44 +0000

    checkpoint models

--- src/train.py
+++ src/train.py
@@ -1,38 +1,47 @@
 import hydra
 import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.plugins import DDPPlugin
 
 from datamodules import UNETDataModule
 from model import UNETModel
-from utils import load_hpu_library, set_env_params
+from utils import PeriodicCheckpoint, load_hpu_library, set_env_params
 
 
 @hydra.main(config_path="configs", config_name="config")
 def main(cfg):
 
-    set_env_params(run_lazy_mode=False, hpus_per_node=cfg.training.cores)
+    set_env_params(
+        run_lazy_mode=cfg.training.run_lazy_mode, hpus_per_node=cfg.training.cores
+    )
     load_hpu_library()
 
     unet_data = UNETDataModule(config=cfg)
     unet_model = UNETModel(cfg=cfg)
-
-    check_point = pl.callbacks.ModelCheckpoint(
-        dirpath="checkpoints",
-        filename="model_checkpoint_{epoch}",
-        save_top_k=3,
-        verbose=True,
-        monitor="valid/loss",
-        mode="min",
-        save_on_train_epoch_end=True,
+    model_ckpt = PeriodicCheckpoint(
+        monitor="valid/dice_score",
+        mode="max",
+        save_last=True,
+        every_n=cfg.training.ckpt_every,
+        pl_module=unet_model,
     )
-    wandb_logger = WandbLogger(project="hpu" + "/saved/")
+    early_stopping = EarlyStopping(
+        monitor="valid/dice_score",
+        patience=cfg.training.patience,
+        verbose=True,
+        mode="max",
+    )
+    wandb_logger = WandbLogger(
+        project="dltb_hpu", save_dir=hydra.utils.get_original_cwd() + "/saved/"
+    )
+    parallel_hpus = [torch.device("hpu")] * cfg.training.cores
     unet_trainer = pl.Trainer(
         hpus=cfg.training.cores if cfg.training.device else None,
         precision=16,
         logger=wandb_logger,
-        callbacks=[
-            check_point,
-        ],
+        callbacks=[model_ckpt, early_stopping],
         default_root_dir=cfg.training.save_dir,
         fast_dev_run=False,
         limit_train_batches=cfg.training.limit_train_batches,
@@ -40,7 +49,16 @@ def main(cfg):
         max_epochs=cfg.training.max_epochs,
         log_every_n_steps=1,
         deterministic=cfg.training.deterministic,
+        strategy=DDPPlugin(
+            parallel_devices=parallel_hpus,
+            bucket_cap_mb=cfg.training.bucket_cap_mb,
+            gradient_as_bucket_view=True,
+            static_graph=True,
+        )
+        if cfg.training.cores > 1
+        else None,
     )
+    # with torch.autograd.profiler.emit_nvtx():
     unet_trainer.fit(unet_model, unet_data)
 
 
